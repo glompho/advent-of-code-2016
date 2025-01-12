265138d7dce7e6e05a9758dab75575387b141bd4
glomph <glompho@gmail.com>
2025-01-12 22:22:17 +0000

    Hardest part was thinking about ascii codes

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,66 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_input) do
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.trim()
+      |> String.split(["[", "]"], trim: true)
+    end)
+    |> Enum.map(fn [name, checksum] ->
+      [[id]] = Regex.scan(~r/\d+/, name)
+
+      {result, _rest} =
+        name
+        |> String.replace("-", "")
+        |> then(&Regex.replace(~r/\d+|-/, &1, ""))
+        |> String.graphemes()
+        |> Enum.frequencies()
+        |> Enum.sort_by(fn {char, freq} -> {1000 - freq, char} end)
+        |> Enum.map(fn {char, _freq} -> char end)
+        |> Enum.join("")
+        |> String.split_at(5)
+
+      {name, result, checksum, id}
+    end)
   end
 
-  def part2(_input) do
+  def part1(input) do
+    input
+    |> parse()
+    |> Enum.reduce(0, fn {_name, result, checksum, id}, acc ->
+      # IO.inspect({result, checksum, id, result == checksum})
+
+      if result == checksum do
+        acc + String.to_integer(id)
+      else
+        acc
+      end
+    end)
+  end
+
+  def decode(name, id) do
+    name
+    |> String.to_charlist()
+    |> Enum.map(fn charcode ->
+      case charcode do
+        ?- ->
+          32
+
+        _ ->
+          96 + rem(charcode - 96 + String.to_integer(id), 26)
+      end
+    end)
+  end
+
+  def part2(input) do
+    input
+    |> parse()
+    |> Enum.map(fn {name, _result, _checksum, id} ->
+      # IO.inspect({result, checksum, id, result == checksum})
+      {"#{decode(name, id)}", id}
+    end)
+    |> Enum.find(fn {decoded, _id} -> String.contains?(decoded, "northpole") end)
+    |> elem(1)
   end
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,17 +3,22 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "aaaaa-bbb-z-y-x-123[abxyz]
+    a-b-c-d-e-f-g-h-987[abcde]
+    not-a-real-room-404[oarel]
+    totally-real-room-200[decoy]"
     result = part1(input)
 
-    assert result
+    assert result == 1514
   end
 
   @tag :skip
   test "part2" do
-    input = nil
+    input = "aaaaa-bbb-z-y-x-123[abxyz]
+    a-b-c-d-e-f-g-h-987[abcde]
+    not-a-real-room-404[oarel]
+    totally-real-room-200[decoy]"
     result = part2(input)
 
     assert result
