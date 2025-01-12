9cc8f67c045c95f0c1f6379c92a388900eadb0fa
glomph <glompho@gmail.com>
2025-01-12 22:36:43 +0000

    Feels like Enum.frequencies basically did this one for me

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,23 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_input) do
+  def part1(input, sort_function \\ &>/2) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line |> String.trim() |> String.graphemes()
+    end)
+    |> Stream.zip()
+    |> Enum.map(fn col ->
+      col
+      |> Tuple.to_list()
+      |> Enum.frequencies()
+      |> Enum.sort_by(&elem(&1, 1), sort_function)
+      |> hd()
+      |> elem(0)
+    end)
+    |> Enum.join("")
   end
 
-  def part2(_input) do
+  def part2(input) do
+    part1(input, &</2)
   end
 end
